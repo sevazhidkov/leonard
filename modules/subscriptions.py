2c23f00714ffbb1baee1f5595f87a71de01d78e3
elishanto <elishanto@gmail.com>
2016-10-22 19:28:43 +0300

    Refactoring

--- modules/subscriptions.py
+++ modules/subscriptions.py
@@ -2,7 +2,6 @@ import telegram
 import jinja2
 
 from leonard import Leonard
-from modules import weather
 
 INITIAL_WEATHER_OFFER = jinja2.Template("""You shouldn't write me to get help – you can subscribe to some notification messages.
 
@@ -10,7 +9,7 @@ INITIAL_WEATHER_OFFER = jinja2.Template("""You shouldn't write me to get help �
 """)
 WEATHER_SETUP_RESULT = jinja2.Template(
     "👌 Not a problem, {% if result %}I subscribed you. It's ready " +
-    "and will notify you next time.{% else %}you can always change it later.{% endif %}"
+    "and will notificate you next time.{% else %}you can always change it later.{% endif %}"
 )
 
 
@@ -29,14 +28,14 @@ def subscriptions_setup(message, bot):
         text=INITIAL_WEATHER_OFFER.render(),
         reply_markup=telegram.ReplyKeyboardMarkup(
             [['Yeah, send me morning forecasts 🌄'],
-             ['Notify me about upcoming rain ☔️'],
+             ['Notificate me about upcoming rain ☔️'],
              ['No, thanks, I will setup it later 🚫']]
         )
     )
 
 
 def subscriptions_setup_result(message, bot):
-    base_key = 'notifications:{}:{}'.format(weather.NAME, '{}')
+    base_key = 'notifications:weather:{}'
     if '🌄' in message.text:
         key = base_key.format('morning')
     elif '☔️' in message.text:
@@ -50,51 +49,4 @@ def subscriptions_setup_result(message, bot):
     )
     if key:
         bot.user_set(message.u_id, key, 1)
-        bot.redis.expire('user:{}:{}'.format(message.u_id, key), 24 * 60 * 60)
     bot.call_handler(message, 'welcome-location-setup')
-
-
-def show_subscriptions(message, bot: Leonard):
-    bot.user_set(message.u_id, 'next_handler', 'subscriptions-choose')
-    reply_markup = telegram.ReplyKeyboardMarkup(
-        [[telegram.KeyboardButton(subscription)] for subscription in bot.available_subscriptions.keys()]
-    )
-    reply_markup.keyboard.append([telegram.KeyboardButton(bot.MENU_BUTTON)])
-    bot.telegram.send_message(message.u_id, 'There are {} subscribe sources'.format(len(bot.available_subscriptions)),
-                              reply_markup=reply_markup, parse_mode=telegram.ParseMode.MARKDOWN)
-    pass
-
-
-def choose_subscriptions(message, bot: Leonard):
-    if message.text in bot.available_subscriptions:
-        bot.user_set(message.u_id, 'next_handler', 'subscription-set')
-        chosen_subscriptions = bot.available_subscriptions[message.text]
-        reply_markup = telegram.ReplyKeyboardMarkup(
-            [[telegram.KeyboardButton('{} - {}'.format(message.text, subscription))] for subscription in
-             chosen_subscriptions]
-        )
-        reply_markup.keyboard.append([telegram.KeyboardButton(bot.MENU_BUTTON)])
-        bot.telegram.send_message(message.u_id, 'There are {} subscription types'.format(len(chosen_subscriptions)),
-                                  reply_markup=reply_markup, parse_mode=telegram.ParseMode.MARKDOWN)
-    else:
-        bot.call_handler(message, 'main-menu')
-
-
-def set_subscription(message, bot: Leonard):
-    if len(message.text.split(' - ')) <= 1:
-        bot.call_handler(message, 'main-menu')
-        return
-    plugin, subscription = message.text.split(' - ')
-    if plugin not in bot.available_subscriptions:
-        bot.call_handler(message, 'main-menu')
-        return
-
-    if subscription not in bot.available_subscriptions[plugin]:
-        bot.call_handler(message, 'main-menu')
-        return
-
-    subscription = bot.available_subscriptions[plugin][subscription]
-    bot.user_set(message.u_id, 'notifications:{}:{}'.format(plugin, subscription), 1)
-    bot.telegram.send_message(message.u_id, 'You have been successfully subscribed to "{}"'.format(message.text),
-                              reply_markup=telegram.ReplyKeyboardHide(), parse_mode=telegram.ParseMode.MARKDOWN)
-    bot.call_handler(message, 'main-menu')
