ae8336d03c0276d945da4078728e36ea451bdc0b
elishanto <elishanto@gmail.com>
2016-10-23 17:51:36 +0300

    Add morning and evening weather subscriptions

--- modules/weather.py
+++ modules/weather.py
@@ -6,7 +6,7 @@ import requests
 import telegram
 import jinja2
 import pytz
-import datetime
+import time
 
 from leonard import Leonard
 from modules.location import set_location
@@ -38,6 +38,7 @@ WEATHER_ICONS = {
 
 SUBSCRIBES = collections.OrderedDict([
     ('Every morning', 'morning'),
+    ('Every evening', 'evening'),
     ('Before rain', 'rain'),
 ])
 
@@ -49,18 +50,38 @@ def register(bot):
     bot.handlers['weather-rain'] = morning_forecast
 
     bot.subscriptions.append(('{}:morning'.format(NAME), check_show_weather_morning, send_show_weather))
+    bot.subscriptions.append(('{}:evening'.format(NAME), check_show_weather_evening, send_show_weather))
     # bot.subscriptions.append(('{}:rain'.format(NAME), check_show_weather_rain, send_show_weather))
 
 
-def check_show_weather_morning(bot):
+def check_show_weather_morning(bot: Leonard):
+    users = bot.redis.keys('user:*notifications:{}:{}'.format(NAME, 'morning'))
+    return check_show_weather_condition(
+        bot,
+        lambda timezone: ('morning', arrow.now(timezone).datetime.hour in (8, 9, 10)),
+        map(lambda x: x.decode('utf-8').split(':')[1], users) if users else []
+    )
+
+
+def check_show_weather_evening(bot: Leonard):
+    users = bot.redis.keys('user:*notifications:{}:{}'.format(NAME, 'evening'))
+    return check_show_weather_condition(
+        bot,
+        lambda timezone: ('evening', arrow.now(timezone).datetime.hour in (19, 20, 21)),
+        map(lambda x: x.decode('utf-8').split(':')[1], users) if users else []
+    )
+
+
+def check_show_weather_condition(bot: Leonard, condition, users):
     result = []
-    for user in map(lambda x: x.decode('utf-8'), bot.redis.keys('user:*:location')):
-        u_id = user.split(':')[1]
-        user = eval(bot.redis.get(user).decode('utf-8'))
+    for u_id in users:
+        user = eval(bot.redis.get('user:{}:location'.format(u_id)).decode('utf-8'))
         timezone = pytz.timezone(user['timezone'])
-        time = datetime.datetime.now(timezone)
-        if time.hour == 10:
+        name, correct = condition(timezone)
+        if correct and int(time.time() * 1000) - \
+                int(bot.user_get(u_id, 'notifications:{}:{}:last'.format(NAME, name)) or 0) > 86400000:
             result.append(int(u_id))
+            bot.user_set(u_id, 'notifications:{}:{}:last'.format(NAME, name), int(time.time() * 1000))
     return result
 
 
