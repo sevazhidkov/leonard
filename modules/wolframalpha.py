c826ecae4e80ffedf6ff910fb1078a8da0860323
elishanto <elishanto@gmail.com>
2016-11-07 21:56:04 +0300

    Add main menu call after wolfram usage

---
+++ modules/wolframalpha.py
@@ -0,0 +1,44 @@
+import os
+from urllib.parse import quote_plus
+
+import jinja2
+import wolframalpha
+from telegram import InlineKeyboardButton
+from telegram import InlineKeyboardMarkup
+
+from leonard import Leonard
+
+
+def register(bot):
+    bot.wolfram_client = wolframalpha.Client(os.environ['WOLPHRAM_ALPHA_KEY'])
+    bot.handlers['wolfram-ask'] = ask_wolfram
+    bot.handlers['result-get'] = get_result
+
+
+def ask_wolfram(message, bot: Leonard):
+    bot.user_set(message.u_id, 'next_handler', 'result-get')
+    bot.send_message(message.u_id, 'What do you want to calculate or know about? 🤔')
+
+
+def get_result(message, bot: Leonard):
+    response = bot.wolfram_client.query(message.text)
+    url = 'https://www.wolframalpha.com/input/?i=' + quote_plus(message.text)
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton('Open on WolphramAlpha.com', url=url)]])
+    res = [pod for pod in response.pods if
+           pod.title.lower() in ('result', 'response', 'description', 'root', 'solution')]
+    if len(res) > 0 and not any(v is None for v in res):
+        if res[0].text is not None:
+            bot.telegram.send_message(message.u_id, '\n'.join(list(map(lambda x: x.text, res))),
+                                      reply_markup=reply_markup)
+        elif res[0].img is not None:
+            bot.telegram.send_photo(message.u_id, photo=res[0].img, reply_markup=reply_markup)
+        else:
+            bot.telegram.send_message(message.u_id, 'Wolfram Alpha doesn\'t know anything about that 😢',
+                                      reply_markup=reply_markup)
+    elif any('plot' in x.title.lower() for x in response.pods):
+        bot.telegram.send_photo(message.u_id, photo=[x for x in response.pods if 'plot' in x.title.lower()][0].img,
+                                reply_markup=reply_markup)
+    else:
+        bot.telegram.send_message(message.u_id, 'Wolfram Alpha doesn\'t know anything about that 😢',
+                                  reply_markup=reply_markup)
+    bot.call_handler(message, 'main-menu')
