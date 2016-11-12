bc95b74711b7dd393343bd47d99ab6bbc7ea8b4c
elishanto <elishanto@gmail.com>
2016-11-12 17:12:04 +0300

    Add 9GAG posts cleaner

---
+++ jobs/clean_9gag_posts.py
@@ -0,0 +1,26 @@
+import time
+import boto3
+import logging
+
+from boto3.dynamodb.conditions import Key
+
+
+def main():
+    table = boto3.resource('dynamodb', 'eu-west-1').Table('LeonardBot9gagPosts')
+    response = table.scan(
+        FilterExpression=Key('createdAt').lt(int(time.time()) - 604800)
+    )
+    logging.info('{} memes to delete'.format(response['Count']))
+    for meme in response['Items']:
+        table.delete_item(
+            Key={
+                'postId': meme['postId']
+            }
+        )
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception as e:
+        logging.error(e)
